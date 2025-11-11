536f9802180f3f7c741116a0c15479be261070bf
Dev-Soares <bernardo.soares30@outlook.com>
2025-11-11 18:01:57 -0300

    fix: fix de permissão de CORS

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: ['https://e-commerce-megashop.vercel.app/', 'http://localhost:5173'] 
+  origin: ['https://e-commerce-megashop.vercel.app', 'http://localhost:5173'] 
 }));
 
 app.use(express.json());
