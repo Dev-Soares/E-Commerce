14098d1517cc0e5e9c7cac71f60681b88f273c8d
Dev-Soares <bernardo.soares30@outlook.com>
2025-11-27 23:10:51 -0300

    fix de CORS

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,24 @@
-import express from 'express';
-import ProductListRouter from './routes/ProductListRouter.js';
-import CartListRouter from './routes/CartListRouter.js';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import ProductListRouter from "./routes/ProductListRouter.js";
+import CartListRouter from "./routes/CartListRouter.js";
+import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 
 dotenv.config();
 
-app.use(cors({
-  origin: ['https://e-commerce-megashop.vercel.app', 'http://localhost:5173'] 
-}));
+app.use(
+  cors({
+    origin: [
+      "https://e-commerce-megashop.vercel.app",
+       "http://localhost:5173"
+    ]
+  })
+);
 
 app.use(express.json());
-app.use('/api', ProductListRouter);
-app.use('/api', CartListRouter);
+app.use("/api", ProductListRouter);
+app.use("/api", CartListRouter);
 
 export default app;
-
